bd70cd43e254b5c9689f55107828b77ce7fa619a
AaronJau21 <aaronjau@gmail.com>
2023-05-28 06:59:37 -0700

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-''' Export data in the JSON format '''
+"""Write a Python script that, using this REST"""
 import json
-import requests as r  # give an alias to the module
+from requests import get
 
 
-def get_api():
-    ''' Gather data from an API '''
-    url = "https://jsonplaceholder.typicode.com/"
-    user = r.get(url + "users").json()
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = get("{}users".format(url)).json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({usr.get("id"): [{
-            "username": usr.get("username"),
-            "task": e.get("title"),
-            "completed": e.get("completed")
-        } for e in r.get(url + "todos",
-                         params={"userId": usr.get("id")}).json()]
-            for usr in user}, jsonfile)
-
-
-if __name__ == '__main__':
-    get_api()
+    all_employed = {}
+    for user in users:
+        id_user = user.get('id')
+        username = user.get('username')
+        tasks = get('{}todos?userId={}'.format(url, id_user)).json()
+        dict_task = [{"username": username,
+                      "task": task['title'],
+                      "completed": task['completed']} for task in tasks]
+        all_employed[id_user] = dict_task
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as file:
+        json.dump(all_employed, file)
